6d79789ee029fbea3a8414ab2f0f83948af3820e
LeeKann <erickannam@gmail.com>
2017-04-19 17:45:15 -0700

    Zillow api route and internal code added Returns a javascript object with addresses and zillow estimated prices

---
+++ internal/zillow.js
@@ -0,0 +1,23 @@
+const Zillow = require('node-zillow');
+
+const zwisd = process.env.ZWISD;
+
+const zillow = new Zillow(zwisd);
+
+const getNeighbours = function getNeighbours(parameters) {
+  zillow.get('GetSearchResults', parameters).then((result) => {
+
+    const zpids = result.response.results.result[0].zpid;
+    const p = { zpid: zpids, count: 10 };
+    const second = zillow.get('GetComps', p);
+    const results = [];
+    second.then((neighbours) => {
+      const comps = neighbours.response.properties.comparables[0].comp;
+
+      comps.forEach((comp) => {
+        results.push({ address: comp.address, zestimate: comp.zestimate });
+      });
+    });
+      return results;
+  });
+};
